de4d60b7672ea93f78b2187d2f8f392facdfacee
pnakibar <pedronakibar@gmail.com>
2015-07-29 19:25:49 -0300

    TUDO DOMINADO

---
+++ file.py
@@ -0,0 +1,12 @@
+f = open("lucas", 'r')
+
+for _ in range(3):
+	f.readline().split()
+
+
+
+
+
+1 2 3
+4 5 6
+7 8 9
--- grafos2.py
+++ grafos2.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: Latin1 -*-
+import sys
+
 def load(filepath):
     f = open(filepath, 'r')
 
@@ -12,7 +16,7 @@ def load(filepath):
     matriz = []
 
     for _ in range(numVertices):
-        matriz.append(f.readline().replace("\r\n", "").split())
+        matriz.append(map(lambda x: int(x), f.readline().replace("\r\n", "").split()))
 
     return rotulos, matriz
 
@@ -21,17 +25,18 @@ def load(filepath):
 def adjacente(G, v):
     adj = []
     for e in range(len(G[v])):
-        if G[e] > 0:
+        if G[v][e] > 0:
             adj.append(e)
 
     return adj
 
-def DFS(matriz, rotulos):
+def DFS(matriz, rotulos, ordtop=False):
     pred = {}
     cor = {}
     d = {}
     t = {}
     tempo = [0]
+    LISTA_ORDENACAO_TOPOLOGICA = []
 
     def visita(u): #u -> vertice
         cor[u] = "cinza"
@@ -45,6 +50,7 @@ def DFS(matriz, rotulos):
         cor[u] = 'preto'
         tempo[0] += 1
         t[u] = tempo
+        LISTA_ORDENACAO_TOPOLOGICA.append(u)
 
     #para cada vertice do grafo
     for u in range(len(rotulos)): #u -> vertice
@@ -56,12 +62,17 @@ def DFS(matriz, rotulos):
         if cor[u] == "branco":
             visita(u)
 
-    #print("pred", pred)
-    #print("cor", cor)
-    #print("d", d)
-    #print("t", t)
-    #print("tempo", tempo[0])
-    return pred
+    '''
+    print("pred", pred)
+    print("cor", cor)
+    print("d", d)
+    print("t", t)
+    print("tempo", tempo[0])
+    '''
+    if ordtop:
+        return LISTA_ORDENACAO_TOPOLOGICA
+    else:
+        return pred
 
 def BFS(matriz, rotulos):
     cor = {}
@@ -88,25 +99,30 @@ def BFS(matriz, rotulos):
         d[u] = float('inf')
         antecessor[u] = None
 
+    d[range(len(rotulos))[0]] = 0 #coloca o primeiro com peso 0
     for u in range(len(rotulos)):
         if cor[u] == "branco":
             innerBFS(u)
-
+    '''
     print("cor", cor)
     print("antecessor", antecessor)
     print("d", d)
     print("Q", Q)
+    '''
+    return antecessor
 
-def relaxamento(matriz, rotulos):
+
+def relaxamento(matriz, rotulos, distance):
     predecessor = {}
-    distance = {}
     #passo2: relaxamento
     for i in range(len(rotulos)):
-        for j in adjacente(matriz, i)
-            peso = matriz[i][j]
+        for j in adjacente(matriz, i):
+            peso = int(matriz[i][j])
+
             if distance[i]  + peso < distance[j]:
                 distance[j] = distance[i] + peso
                 predecessor[j] = i
+
     return predecessor, distance
 
 
@@ -127,37 +143,47 @@ def bellmanFord(matriz, rotulos, s):
     #passo2: relaxamento
     '''
     for i in range(len(rotulos)):
-        for j in adjacente(matriz, i)
+        for j in adjacente(matriz, i):
             peso = matriz[i][j]
             if distance[i]  + peso < distance[j]:
                 distance[j] = distance[i] + peso
                 predecessor[j] = i
     '''
-    predecessor, distance = relaxamento(matriz, rotulos)
+    predecessor, distance = relaxamento(matriz, rotulos, distance)
 
     #passo3
     for i in range(len(rotulos)):
         for j in adjacente(matriz, i):
-            peso = matriz.[i][j]
+            peso = matriz[i][j]
             if distance[i] + peso < distance[j]:
                 raise Exception("Graph contains a negative-weight cycle")
 
     return predecessor
 
-def caminhoMaisCurtoGao(matriz, rotulos, s):
-    dist = {}
-    predecessor = DFS(matriz, rotulos)
 
+def caminhoMaisCurtoGao(matriz, rotulos, s):
+    d = {}
+    lot = DFS(matriz, rotulos, ordtop=True) #TODO: ordenar pelo valor
+    predecessor = {}
 
     #inicializar origem unica
-    dist[s] = 0
-    for e in predecessor:
-        if e != s:
-            predecessor[e] = -1
-            dist[e] = float("inf")
+    for v in range(len(rotulos)):
+        predecessor[v] = None
+        d[v] = float("inf")
+    d[s] = 0
 
     #relaxamento
-    predecessor, distancia = relaxamento(matriz, rotulos)
+    print("lot", lot)
+    for u in lot:
+        print("u:", str(u), "adjacente: ", adjacente(matriz,u))
+        for v in adjacente(matriz, u):
+            if d[v] > d[u] + matriz[u][v]:
+                d[v] = d[u] + matriz[u][v]
+                predecessor[v] = u
+
+    print("predecessor", predecessor)
+    print("d", d)
+
 
 #s->no inicial
 def removeMenor(l):
@@ -181,12 +207,26 @@ def prim(matriz, rotulos, s):
 
     while (len(Q) > 0):
         u = removeMenor(Q)
-        for v in adjacente(G, u):
+        for v in adjacente(matriz, u):
             if (v in Q) and matriz[u][v] < custo[v]:
                 p[v] = u
                 custo[v] = matriz[u][v]
+    print(custo)
+    print(p)
 
-rotulos, matriz = load("gustavera.txt")
-DFS(matriz, rotulos)
-BFS(matriz, rotulos)
-bellmanFord(matriz, rotulos, 0)
+
+rotulos, matriz = load(sys.argv[1])
+'''
+print("dfs:")
+print(DFS(matriz, rotulos))
+print("bfs:")
+print(BFS(matriz, rotulos))
+print("bellmanFord:")
+print(bellmanFord(matriz, rotulos, 0))
+'''
+print("caminhoMaisCurtoGao")
+print(caminhoMaisCurtoGao(matriz, rotulos, 0))
+'''
+print("prim")
+print(prim(matriz, rotulos, 0))
+'''
